23d1b92d645dbc7d1f56e5f689c68a53e3036327
cshfym <cshfym@gmail.com>
2017-09-25 20:55:26 -0600

    Launch pre-fetch of VSE on scheduled timer.

--- src/main/groovy/com/gastocks/server/Application.groovy
+++ src/main/groovy/com/gastocks/server/Application.groovy
@@ -3,8 +3,6 @@ package com.gastocks.server
 import com.gastocks.server.config.CacheConfiguration
 import com.gastocks.server.config.TaskExecutorConfiguration
 import com.gastocks.server.jms.config.JmsConfiguration
-import com.gastocks.server.services.PreFetchService
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
@@ -13,17 +11,14 @@ import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.web.support.SpringBootServletInitializer
 import org.springframework.context.ApplicationContext
-import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.EnableAspectJAutoProxy
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.PropertySource
 import org.springframework.context.annotation.PropertySources
 import org.springframework.jms.annotation.EnableJms
 import org.springframework.scheduling.annotation.EnableScheduling
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import org.springframework.web.servlet.DispatcherServlet
 
-import javax.annotation.PostConstruct
 
 @EnableJms
 @EnableScheduling
--- src/main/groovy/com/gastocks/server/services/PreFetchService.groovy
+++ src/main/groovy/com/gastocks/server/services/PreFetchService.groovy
@@ -1,41 +1,22 @@
 package com.gastocks.server.services
 
-import com.gastocks.server.models.domain.PersistableSymbol
-import com.gastocks.server.services.domain.QuotePersistenceService
-import com.gastocks.server.services.domain.SymbolPersistenceService
+import com.gastocks.server.services.domain.ViewSymbolExtendedCacheService
+import groovy.util.logging.Slf4j
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.core.task.TaskExecutor
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 
+@Slf4j
 @Component
 class PreFetchService {
 
     @Autowired
-    private TaskExecutor taskExecutor
+    ViewSymbolExtendedCacheService viewSymbolExtendedCacheService
 
-    @Autowired
-    QuotePersistenceService quotePersistenceService
-
-    @Autowired
-    SymbolPersistenceService symbolPersistenceService
-
-    void preFetch() {
-
-        // Do some work here.
+    @Scheduled(fixedDelay = 43200000L, initialDelay = 10000L) // Every 12 hours, delay 10s on startup.
+    void preFetchViewSymbolExtended() {
+        log.info("Triggering pre-fetch of viewSymbolExtended")
+        viewSymbolExtendedCacheService.findAllViewSymbolExtendedFromCache()
     }
-
-    /* Unused
-    private class QuoteFetchClass implements Runnable {
-
-        PersistableSymbol symbol
-
-        QuoteFetchClass(PersistableSymbol symbol) { this.symbol = symbol }
-
-        void run() {
-            quotePersistenceService.findAllQuotesForSymbol(this.symbol) // Method is cached
-        }
-
-    }
-    */
 
 }
