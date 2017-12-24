38e9b1f6f4aeef8fe6790f3b7dbb7ed82ee1640b
Chuck <cshfym@gmail.com>
2017-12-24 16:09:48 -0700

    DB tweaks.

--- src/main/resources/changelog/changelog-1.0.0.quote.sql
+++ src/main/resources/changelog/changelog-1.0.0.quote.sql
@@ -1,7 +1,6 @@
 start transaction;
 
 use `ga_stocks`;
-
 CREATE TABLE `quote` (
   `id` varchar(36) COLLATE utf8mb4_bin NOT NULL,
   `symbol_id` varchar(36) COLLATE utf8mb4_bin NOT NULL,
@@ -11,7 +10,7 @@ CREATE TABLE `quote` (
   `day_low` double(9,3) DEFAULT '0.000',
   `previous_day_close` double(9,3) DEFAULT '0.000',
   `price_change` double(9,3) DEFAULT '0.000',
-  `price_change_percentage` float(7,4) DEFAULT '0.0000',
+  `price_change_percentage` float(10,6) DEFAULT '0.000000',
   `volume` int(11) DEFAULT '0',
   `quote_date` date DEFAULT NULL,
   `dividend` double(9,3) DEFAULT '0.000',
@@ -20,6 +19,7 @@ CREATE TABLE `quote` (
   UNIQUE KEY `IDX_SymbolId_QuoteDate` (`symbol_id`,`quote_date`),
   KEY `FK_Quote_Symbol` (`symbol_id`),
   KEY `IDX_SymbolID_QuoteDate_Price` (`symbol_id`,`quote_date`,`price`),
+  KEY `idx_quote_quote_date` (`quote_date`),
   CONSTRAINT `FK_Quote_Symbol` FOREIGN KEY (`symbol_id`) REFERENCES `symbol` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
